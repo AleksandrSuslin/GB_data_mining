ff249ceeb61ecc0e6f505dc6833019e3b340ac89
AleksandrSuslin <suslin.aleksandr@bk.ru>
2022-06-18 12:46:31 +0300

    Lesson 6.1

---
+++ Lesson 6/settings.py
@@ -0,0 +1,31 @@
+BOT_NAME = 'booksparser'
+
+SPIDER_MODULES = ['booksparser.spiders']
+NEWSPIDER_MODULE = 'booksparser.spiders'
+
+
+
+USER_AGENT = "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64)"
+                        " AppleWebKit/537.36 (KHTML, like Gecko)"
+                        " Chrome/101.0.4951.54 Safari/537.36"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+LOG_ENABLED = True
+LOG_LEVEL = "DEBUG"
+
+
+CONCURRENT_REQUESTS = 8
+
+
+DOWNLOAD_DELAY = 1.5
+)
+COOKIES_ENABLED = True
+
+
+
+
+ITEM_PIPELINES = {
+   'booksparser.pipelines.BooksparserPipeline': 300,
+}
